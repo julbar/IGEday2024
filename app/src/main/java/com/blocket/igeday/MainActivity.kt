568f5a346edd5686826daef1ebb18e568bcb8f7d
julita.barkauskaite@schibsted.com <julita.barkauskaite@schibsted.com>
2024-03-22 14:48:22 +0100

    Workshop part1 & part2

--- app/src/main/java/com/blocket/igeday/MainActivity.kt
+++ app/src/main/java/com/blocket/igeday/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.blocket.igeday
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.button_next).setOnClickListener {
+            startActivity(Intent(this, NextActivity::class.java), )
+        }
     }
 }
--- app/src/main/java/com/blocket/igeday/NextActivity.kt
+++ app/src/main/java/com/blocket/igeday/NextActivity.kt
@@ -1,11 +1,33 @@
 package com.blocket.igeday
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class NextActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_next)
+
+        val priceText = findViewById<TextView>(R.id.text_price)
+        val plusButton = findViewById<Button>(R.id.button_plus)
+        val minusButton =  findViewById<Button>(R.id.button_minus)
+
+        var price: Int = 2000
+        var newPrice: Int = 0
+        val priceString : String = "Pris: "
+
+        plusButton.setOnClickListener {
+            newPrice = ++price
+            price = newPrice
+            priceText.setText(priceString + price.toString())
+        }
+
+        minusButton.setOnClickListener {
+            newPrice = --price
+            price = newPrice
+            priceText.setText(priceString + price.toString())
+        }
     }
 }
